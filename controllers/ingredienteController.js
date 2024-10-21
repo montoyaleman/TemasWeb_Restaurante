a02fe7d356646a5077a5a77b39057e775724cfd5
Montoya Aleman <montoyaleman167@gmail.com>
2024-10-21 10:45:05 -0700

    validacion + codigos http de ingredientes

--- controllers/ingredienteController.js
+++ controllers/ingredienteController.js
@@ -20,7 +20,7 @@ exports.getAllIngredientes = async (req, res) => {
     try {
       await connectarMongoose();
       const restauranteData = await Ingrediente.find().exec(); 
-      res.json(restauranteData);
+      res.status(200).json(restauranteData);
       //mongoose.disconnect();
     } catch (err) {
       console.log(err)
@@ -34,8 +34,9 @@ exports.getIngredienteById = async (req, res) => {
     console.log(ingredienteId);  
     try {
       await connectarMongoose();
-      const ingredienteData = await Ingrediente.find({_id: ingredienteId}).exec();  
-      res.json(ingredienteData);
+      const ingredienteData = await Ingrediente.find({_id: ingredienteId}).exec();
+      if (!ingredienteData) res.status(404).json("No se encontro un ingrediente con ese ID.");
+      else res.status(200).json(ingredienteData);
       //mongoose.disconnect();
     } catch (err) {
       res.status(500).json({ error: 'No se pudo obtener el Ingrediente' });
@@ -43,16 +44,17 @@ exports.getIngredienteById = async (req, res) => {
   };
   
   exports.addIngrediente = async (req, res) => {
+    if (!req.body.nombre || !req.body.medida || !req.body.cantidad) 
+      return res.status(400).json("No se inserto valores correctos para un ingrediente");
     try {
       await connectarMongoose();
-      const newIngrediente = new Ingrediente(
-      {
+      const newIngrediente = new Ingrediente({
         nombre: req.body.nombre, 
         medida: req.body.medida, 
         cantidad: req.body.cantidad
       });
       await newIngrediente.save();
-      res.json(newIngrediente);
+      res.status(201).json(newIngrediente);
       //mongoose.disconnect();
     } catch (err) {
       res.status(500).json({ error: 'No se pudo agregar el ingrediente' });
@@ -60,12 +62,16 @@ exports.getIngredienteById = async (req, res) => {
   };
   
   exports.updateIngrediente = async (req, res) => {
+    if (isNaN(parseInt(req.params.cantidad))) 
+      return res.status(400).json("Cantidad insertada no valido");
     const filter = { _id: req.params.id };
     const update = { cantidad: req.params.cantidad };
+    
     try {
       await connectarMongoose();
       const updatedIngrediente = await Ingrediente.findOneAndUpdate(filter, update, { new: true });
-      res.json(updatedIngrediente);
+      if (!updatedIngrediente) res.status(404).json("No se encontro un ingrediente con ese ID.");
+      else res.status(200).json(updatedIngrediente);
     } catch (err) {
       console.log(err)
       res.status(500).json({ error: 'No se pudo actualizar el ingrediente' });
@@ -77,9 +83,10 @@ exports.getIngredienteById = async (req, res) => {
     try {
       await connectarMongoose();
       const ingredienteEliminado = await Ingrediente.findOneAndDelete(filter);
-      res.json({ message: 'Ingrediente eliminado exitosamente' });
+      if (!ingredienteEliminado) res.status(404).json("No se encontro un ingrediente con ese ID.");
+      else res.status(200).json({ message: 'Ingrediente eliminado exitosamente' });
     } catch (err) {
-      res.status(500).json({ error: 'No se pudo eliminar el animal' });
+      res.status(500).json({ error: 'No se pudo eliminar el ingrediente' });
     }
   };
   
