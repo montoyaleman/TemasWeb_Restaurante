f2faa98cac9d4ac9694b5bf711e34d4e1704bef2
AnthX0 <victorglezb@gmail.com>
2024-10-23 08:51:55 -0700

    Rutas de usuarioController
    Añadí las rutas a la clase de restaurante.js y comentarios para distinguir las rutas de cada una de las clases de mejor manera.

--- routes/restaurante.js
+++ routes/restaurante.js
@@ -4,6 +4,8 @@ const ingredienteController = require('../models/Ingrediente');
 const platilloController = require('../models/Platillo');
 const pedidoController = require('../models/Pedido')
 
+// Rutas para los ingredientes
+
 // get all ingredientes
 router.get('/ingrediente', ingredienteController.getAllIngredientes);
 //insertar ingredientes
@@ -14,6 +16,8 @@ router.get('/ingrediente/:id', ingredienteController.getIngredienteById);
 router.put('/ingrediente/:id/:cantidad', ingredienteController.updateIngrediente);
 //delete
 router.delete('/ingrediente/:id', ingredienteController.deleteIngrediente);
+
+// Rutas para los platillos
 
 // get all platillos
 router.get('/platillo', platilloController.getAllPlatillos);
@@ -26,6 +30,8 @@ router.put('/platillo/:id', platilloController.updatePlatillo);
 //delete
 router.delete('/platillo/:id', platilloController.deletePlatillo);
 
+// Rutas para los pedidos
+
 // get todos los pedidos
 router.get('/pedido', pedidoController.listarPedidos);
 //insertar pedidos
@@ -34,6 +40,19 @@ router.post('/pedido', pedidoController.crearPedido);
 router.put('/pedido/:id', pedidoController.actualizarPedido);
 //delete
 router.delete('/pedido/:id', pedidoController.eliminarPedido);
+
+// Rutas para usuarios
+
+// Obtener todos los usuarios
+router.get('/usuario', usuarioController.getAllUsuarios);
+// Obtener usuario por ID
+router.get('/usuario/:id', usuarioController.getUsuarioById);
+// Insertar un nuevo usuario
+router.post('/usuario', usuarioController.addUsuario);
+// Actualizar un usuario
+router.put('/usuario/:id', usuarioController.updateUsuario);
+// Eliminar un usuario
+router.delete('/usuario/:id', usuarioController.deleteUsuario);
 
 
 module.exports = router;
