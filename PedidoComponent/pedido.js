a7f7e512996eefdf7212228395f66d408dcad490
jesuscossio <123430791+jesuscossio@users.noreply.github.com>
2024-11-21 10:36:36 -0800

    Cambios a pedido.js

--- PedidoComponent/pedido.js
+++ PedidoComponent/pedido.js
@@ -10,7 +10,7 @@ class Pedido extends HTMLElement {
         this.#agregaEstilo(shadow);
         this.#render(shadow);
         this.#consultaPedido(shadow);
-        //this.#consultaUsuarios(shadow);
+
     }
 
     #agregaEstilo(shadow) {
@@ -22,49 +22,51 @@ class Pedido extends HTMLElement {
 
     #render(shadow) {
         shadow.innerHTML += `
-        <div><h1>Gestión de Pedidos</h1></div>
-        <section class="container">
-            <div class="item">
-                <div class="input">
-                    <h2 class="textosDetalles">ID</h2>
-                    <input type="text" id="txtId>
+        <div> <h1>Gestión de Pedido</h1> </div>		
+		<section class="container">			
+			<div class="item">
+				<div class="input">
+					<h2 class="textosDetalles">ID</h2>
+					<input type="text" id="txtId">
 
-                    <h2 class="textosDetalles">Fecha del Pedido</h2>
-                    <input type="date" id="txtFecha">
+					<h2 class="textosDetalles">Fecha</h2>
+					<input type="date" id="txtFecha">
 
-                    <h2 class="textosDetalles">ID del Usuario</h2>
-                    <input type="text" id="txtIdUsuario">
+					<h2 class="textosDetalles">ID Usuario</h2>
+					<input type="text" id="txtIdUsuario">
 
-                    <h2 class="textosDetalles">Mesa</h2>
-                    <input type="number" id="txtMesa">
+					<h2 class="textosDetalles">Mesa</h2>
+					<input type="text" id="txtMesa">
 
                     <h2 class="textosDetalles">Orden</h2>
-                    <input type="text" id="txtOrden">
-                    
-                    <h2 class="textosDetalles">Precio Total</h2>
-                    <input type="text" id="txtTotal">
-                </div>
-                <div class="botones">
-                    <button type="button" class="button-1" id="btnCrear" onClick="crearPedido()">Crear </button>
-                    <button type="button" class="button-1" id="btnActualizar" onClick="actualizarPedido()">Actualizar</button>
-                    <button type="button" class="button-1" id="btnEliminar" onClick="eliminarPedido()">Eliminar</button>
-                    <button type="button" class="button-1" id="btnBuscarTodos" onClick="listarPedido()">Buscar Todos</button>
-                </div>
-            </div>
-            <div class="item">
-                <div id="bd"></div>
-                    <template id="tmpPedido">
-                        <div class="articuloBd">
-                            <p><b id="idArticulo"></b></p>
-                            <p>Fecha: <span id="fechaArticulo"></span></p>
-                            <p>Id de Usuario: <span id="idUsuarioArticulo"></span></p>
-                            <p>Mesa: <span id="mesaArticulo"></span></p>
-                            <p>Orden: <span id="ordenArticulo"></span></p>
-                            <p>Total: <span id="totalArticulo"></span></p>
-                        </div>
-                     </template>
-            </div>
-        </section>`;
+					<input type="text" id="txtOrden">
+
+                    <h2 class="textosDetalles">Total</h2>
+					<input type="text" id="txtTotal">
+				</div>
+				<div class="botones">
+					<button type="button" class="button-1" id="btnCrear" onClick="crearPedido()">Crear</button>
+					<button type="button" class="button-1" id="btnActualizar" onClick="actualizarPedido()">Actualizar</button>
+					<button type="button" class="button-1" id="btnEliminar" onClick="eliminarPedido()">Eliminar</button>
+					<br>
+					<button type="button" class="button-1" id="btnBuscar" onClick="listarPedidos()">Buscar Todos</button>
+				</div>
+			</div>
+			<div class="item">
+				<div id="bd">				
+				</div>
+				<template id="tmpPedido">
+					<div class="articuloBd">
+						<p><b id="idArticulo"></b></p>
+                        <p>Fecha: <span id="fechaArticulo"></span></p>
+                        <p>Id de Usuario: <span id="idUsuarioArticulo"></span></p>
+                        <p>Mesa: <span id="mesaArticulo"></span></p>
+                        <p>Orden: <span id="ordenArticulo"></span></p>
+                        <p>Total: <span id="totalArticulo"></span></p>
+					</div>
+				</template>
+			</div>
+		</section>`;
     }
 
     #consultaPedido(shadow) {
@@ -80,11 +82,8 @@ class Pedido extends HTMLElement {
 
     #despliegaPedido(tmp, div, pedido) {
         let clone = tmp.content.cloneNode(true);
-
-        let element = clone.querySelector("#idArticulo");
-        element.innerHTML = pedido._id;
         
-        element = clone.querySelector("#fechaArticulo");
+        let element = clone.querySelector("#fechaArticulo");
         element.innerHTML = pedido.fecha;
 
         element = clone.querySelector("#idUsuarioArticulo");
