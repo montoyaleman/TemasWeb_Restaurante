c3de741be8d487fda2aa48282019c97772dd2506
jesuscossio <123430791+jesuscossio@users.noreply.github.com>
2024-12-03 03:37:38 -0800

    Actualizacion Navbar Pedido

--- NavbarComponent/navbar.js
+++ NavbarComponent/navbar.js
@@ -23,7 +23,7 @@ class Navbar extends HTMLElement {
 					<a href="index.html" class="logo">Restaurante</a>
 					<a href="ingredientes.html">Ingredientes</a>
 					<a href="platillosHub.html">Platillos</a>
-					<a href="pedido.html">Pedidos</a>
+					<a href="pedidoHub.html">Pedidos</a>
 					<a href="usuariosHub.html">Usuarios</a>
 				</div>
 			</nav>	
